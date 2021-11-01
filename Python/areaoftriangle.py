adbda7f7d3fcfbfd5d3944c6c7ece9b04dc5763f
rdugubati <44612782+rdugubati@users.noreply.github.com>
2021-11-01 11:26:15 +0530

    Create areaoftriangle.py

---
+++ Python/areaoftriangle.py
@@ -0,0 +1,15 @@
+# Python Program to find 
+# the area of right angled triangle
+
+# import math file
+import math
+
+# inputs from the user
+width = float(input('Enter the width of triangle: '))
+height = float(input('Enter the height of triangle: '))
+
+# calculate the area
+area = 0.5 * width * height
+
+# display result
+print('The area of right triangle is %0.2f' %area)
